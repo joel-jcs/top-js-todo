63c7beaf0e68eacc2d9b914f6490c53688759cf5
joel-jcs <joeljcs@protonmail.com>
2024-09-17 17:00:57 -0400

    created TaskController to handle all task objects

---
+++ src/TaskController.js
@@ -0,0 +1,52 @@
+const TaskController = () => {
+    let tasks = [
+        {
+            id: 12345,
+            name: "go shopping",
+            description: "at the mall",
+            dueDate: "today",
+            priority: "high",
+            notes: "text for notes",
+            list: "personal"
+        }
+    ];
+
+    const createTask = (name, description, dueDate, priority, notes, list) => {
+        const task = {
+            id: Date.now(),
+            name,
+            description,
+            dueDate,
+            priority,
+            notes,
+            list
+        }
+
+        tasks.push(task);
+        
+        return task;
+    }
+
+    const getTasks = () => tasks;
+
+    
+    //TO-DO
+    const getTask = (id) => {
+
+    };
+
+    const updateTask = (id) => {
+
+    };
+
+    const deleteTask = (id) => {
+
+    };
+    
+    return {
+        createTask,
+        getTasks,
+    }
+};
+
+export const { createTask, getTasks } = TaskController();
