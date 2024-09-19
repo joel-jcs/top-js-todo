dae0709709f37c9732067cd267d4257bf308aed5
joel-jcs <joeljcs@protonmail.com>
2024-09-19 15:40:40 -0400

    trimmed code to simpli load lists, tasks, and all eventlisteners with new loadeventlisteners function in eventcontroller module

--- src/index.js
+++ src/index.js
@@ -4,15 +4,8 @@ import EventController from "./EventController.js";
 import ListController from "./ListController.js";
 import TaskController from "./TaskController.js";
 
-ScreenController.loadLists();
-
 const defaultList = ListController.getLists()[0];
 
-EventController.addList();
-EventController.openList();
-
+ScreenController.loadLists();
 ScreenController.loadTasks(defaultList);
-
-EventController.editOrDeleteList(defaultList);
-EventController.addTask();
-ScreenController.viewTask(defaultList);
+EventController.loadEventListeners(defaultList);
